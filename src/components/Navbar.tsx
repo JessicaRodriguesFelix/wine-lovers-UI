d21da2696e46cdd784a2e46a01e9aae1dd097c65
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2023-06-30 15:43:23 -0400

    add shopping cart button to nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
-import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <NavbarBs className="bg-white shadow-sm mb-3">
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
-        <Nav>
+        <Nav className="me-auto">
           <Nav.Link to="/" as={NavLink}>
             Home
           </Nav.Link>
@@ -16,6 +16,33 @@ export function Navbar() {
             About
           </Nav.Link>
         </Nav>
+        <Button style={{ position: "relative" }} variant="outline-primary">
+          <svg
+            width="29px"
+            height="29px"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="16.5" cy="18.5" r="1.5" />
+            <circle cx="9.5" cy="18.5" r="1.5" />
+            <path d="M18 16H8a1 1 0 0 1-.958-.713L4.256 6H3a1 1 0 0 1 0-2h2a1 1 0 0 1 .958.713L6.344 6H21a1 1 0 0 1 .937 1.352l-3 8A1 1 0 0 1 18 16zm-9.256-2h8.563l2.25-6H6.944z" />
+          </svg>
+          {/* div below is an indicator for the shopping cart button to display the number of items */}
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            3
+          </div>
+        </Button>
       </Container>
     </NavbarBs>
   );
