ddc874522aa8b95d92ae74e7be6b393e27e9fd39
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2023-06-30 20:24:18 -0400

    retrieve data from API and render it within the store component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,50 @@
+import { useEffect, useState } from "react";
+
 export function Store() {
-  return <h1>Store</h1>;
+  // Add properties expected to receive from the API
+  interface Wine {
+    idDrink: string;
+    strDrink: string;
+    strDrinkThumb: string;
+  }
+  interface WineData {
+    drinks: Wine[];
+    // Add any other properties you expect to receive from the API
+  }
+
+  const [wines, setWines] = useState<WineData>({ drinks: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+        );
+        const data = await response.json();
+        setWines(data);
+      } catch (error) {
+        console.log("Error fetching wine data from API:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>Store</h1>
+      {wines.drinks && wines.drinks.length > 0 ? (
+        <ul>
+          {wines.drinks.map((wine) => (
+            <li key={wine.idDrink}>
+              {wine.strDrink} -
+              <img src={wine.strDrinkThumb} alt={wine.strDrink} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading wines</p>
+      )}
+    </div>
+  );
+
+  // });
 }
